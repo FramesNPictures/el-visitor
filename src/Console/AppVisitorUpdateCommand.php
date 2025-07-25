d5a2abb4c814a6bf586851ee860c0d99ddeab1e1
Chris Cynarski <chris@cynarski.net>
2025-07-26 00:28:29 +0100

    Correct script path in AppVisitorUpdateCommand to reflect new directory structure

--- src/Console/AppVisitorUpdateCommand.php
+++ src/Console/AppVisitorUpdateCommand.php
@@ -7,14 +7,15 @@ use Illuminate\Console\Command;
 class AppVisitorUpdateCommand extends Command
 {
     protected $signature = 'app:visitor:update';
+
     protected $description = 'Update local IP databases';
 
     public function handle()
     {
-        $dataFolder = __DIR__ . '/../../data';
-        $rootFolder = realpath(__DIR__ . '/../..');
+        $dataFolder = __DIR__.'/../../data';
+        $rootFolder = realpath(__DIR__.'/../..');
 
         $this->info('Updating local IP geolocation databases...');
-        shell_exec('cd "' . $rootFolder . '" && ./scripts/update.sh');
+        shell_exec('cd "'.$rootFolder.'" && ./usr/update-mmdb');
     }
 }
