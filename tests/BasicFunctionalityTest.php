685c043d8e04bd3941d45e51c88e87cf9245cc8d
Chris Cynarski <chris@cynarski.net>
2025-12-25 15:17:48 +0000

    Refactor BasicFunctionalityTest: add IP data provider and improve assertions

--- tests/BasicFunctionalityTest.php
+++ tests/BasicFunctionalityTest.php
@@ -7,8 +7,8 @@ class BasicFunctionalityTest extends Orchestra\Testbench\TestCase
 {
     protected function getEnvironmentSetUp($app)
     {
-        // make sure, our .env file is loaded
-        $app->useEnvironmentPath(__DIR__ . '/..');
+        // Make sure, our .env file is loaded
+        $app->useEnvironmentPath(__DIR__.'/..');
         $app->bootstrapWith([LoadEnvironmentVariables::class]);
         parent::getEnvironmentSetUp($app);
     }
@@ -16,24 +16,36 @@ class BasicFunctionalityTest extends Orchestra\Testbench\TestCase
     protected function defineEnvironment($app)
     {
         // Setup default database to use sqlite :memory:
-        tap($app['config'], function (Repository $config) {
-            $config->set('visitor', require __DIR__ . '/../config/visitor.php');
+        tap($app['config'], function (Repository $config): void {
+            $config->set('visitor', require __DIR__.'/../config/visitor.php');
         });
     }
 
-    public function test_getting_data()
+    /**
+     * @dataProvider ipAddressProvider
+     */
+    public function test_getting_data($ip, $expectedVersion): void
     {
-        $_SERVER['REMOTE_ADDR'] = '132.198.200.196';
-        $_SERVER['REMOTE_ADDR'] = '23.228.130.134';
-        $_SERVER['REMOTE_ADDR'] = '62.210.243.153';
-        $_SERVER['REMOTE_ADDR'] = '2607:fb90:e33c:c367:8c19:2ff:fe47:d1bb';
-//        $_SERVER['REMOTE_ADDR'] = '2603:7080:b700:9f4c:fd84:4e0e:3f68:4c7c';
-//        $_SERVER['REMOTE_ADDR'] = '2.24.127.161';
+        $_SERVER['REMOTE_ADDR'] = $ip;
 
-        /** @var \FNP\ElVisitor\Services\VisitorService $s */
-        $s = app(\FNP\ElVisitor\Services\VisitorService::class);
+        /** @var FNP\ElVisitor\Services\VisitorService $s */
+        $s = app(FNP\ElVisitor\Services\VisitorService::class);
         $v = $s->visitor();
 
-        dd($v);
+        $this->assertInstanceOf(FNP\ElVisitor\Models\Visitor::class, $v);
+        $this->assertEquals($ip, $v->ip);
+        $this->assertEquals($expectedVersion, $v->ipVersion);
+    }
+
+    public static function ipAddressProvider(): array
+    {
+        return [
+            ['132.198.200.196', 4],
+            ['23.228.130.134', 4],
+            ['62.210.243.153', 4],
+            ['2607:fb90:e33c:c367:8c19:2ff:fe47:d1bb', 6],
+            ['2603:7080:b700:9f4c:fd84:4e0e:3f68:4c7c', 6],
+            ['2.24.127.161', 4],
+        ];
     }
 }
